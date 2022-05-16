cc5e748e95a480874e71a465ad56cd2105929292
FrancoChanseaud <103380352+FrancoChanseaud@users.noreply.github.com>
2022-05-16 16:50:09 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".toggle");
+const navMenu = document.querySelector(".menu-navegacion");
+
+navToggle.addEventListener ("click",() => {
+navMenu.classList.toggle("visible");
+});
